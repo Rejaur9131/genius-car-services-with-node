7d609ef42c8f0a619c2b3030f6ac793a9642bb34
Rejaur9131 <raju_seu@yahoo.com>
2022-04-24 20:52:30 +0600

    genius-car-services-with-node

---
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import google from '../../../images/social/google.png';
+import facebook from '../../../images/social/facebook.png';
+import github from '../../../images/social/gitHub.png';
+import auth from './../../../firebase.init';
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import Loading from './../../Shared/Loading/Loading';
+
+const SocialLogin = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
+  const navigate = useNavigate();
+  let erorrElement;
+
+  if (loading || loading1) {
+    return <Loading></Loading>;
+  }
+
+  if (error || error1) {
+    erorrElement = (
+      <p className="text-danger">
+        Error: {error?.message} {error1?.message}
+      </p>
+    );
+  }
+
+  if (user || user1) {
+    navigate('/home');
+  }
+
+  return (
+    <div>
+      <div className="d-flex align-items-center">
+        <div style={{ height: '1px' }} className="bg-primary w-50"></div>
+        <p className="mt-2 px-2">or</p>
+        <div style={{ height: '1px' }} className="bg-primary w-50"></div>
+      </div>
+      {erorrElement}
+      <div>
+        <button onClick={() => signInWithGoogle()} className="btn btn-info d-block mx-auto w-50 my-2">
+          <img style={{ width: '30px' }} src={google} alt="" />
+          <span className="px-3">Google Sign In</span>
+        </button>
+        <button className="btn btn-info d-block mx-auto w-50 my-2">
+          <img style={{ width: '30px' }} src={facebook} alt="" />
+          <span className="px-2">Facebook Sign In</span>
+        </button>
+        <button onClick={() => signInWithGithub()} className="btn btn-info d-block mx-auto w-50">
+          <img style={{ width: '30px' }} src={github} alt="" />
+          <span className="px-3">Github Sign In</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SocialLogin;
---
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+const ServiceDetail = () => {
+  const { serviceId } = useParams();
+  const [service, setService] = useState({});
+  useEffect(() => {
+    const url = `http://localhost:5000/service/${serviceId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, [serviceId]);
+  return (
+    <div>
+      <h2>You are about to book: {service.name}</h2>
+      <div className="text-center">
+        <Link to="/checkout">
+          <button className="btn btn-primary">Proceed Checkout</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceDetail;
